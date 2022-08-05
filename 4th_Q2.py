c1b9ca318d6f7f5931029e01e879ca9acf94f79b
ShungCa <110527484+ShungCa@users.noreply.github.com>
2022-08-05 17:35:28 +0900

    Add files via upload
    검토할 문장을 외부저장파일로 부터 입력 받도록 수정함.

--- 4th_Q2.py
+++ 4th_Q2.py
@@ -1,19 +1,9 @@
 ##### Q2. 특정 글자가 전체 글에서 몇개 들어있는지 찾는 함수
 #####     변수에 담긴 글을 함수에 넣어주면 txt 파일로 저장도 함께 되도록
 
-a = """안녕하세요. 반갑습니다. 파이썬 공부는 정말 재밌습니다.
-안녕하세요. 반갑습니다. 파이썬 공부는 정말 재밌습니다.
-안녕하세요. 반갑습니다. 파이썬 공부는 정말 재밌습니다.
-안녕하세요. 반갑습니다. 파이썬 공부는 정말 재밌습니다.
-안녕하세요. 반갑습니다. 파이썬 공부는 정말 재밌습니다."""
+def count_word(fname, fword):
 
-def count_word(a, fword):
-
-    text = open("d:\sample1.txt", 'w', encoding = 'utf-8')
-    text.write(a)
-    text.close()
-
-    text = open("sample1.txt", 'r', encoding = 'utf-8')
+    text = open(fname, 'r', encoding = 'utf-8')
     count = 0
 
     for line in text:
@@ -25,4 +15,6 @@ def count_word(a, fword):
 
     print(fword, count)
 
-count_word(a, "습니다")
+fname = "sample.txt"       # fname = input("파일명 입력 : ")
+fword = "파이썬"           # fword = input("단어   입력 : ")
+count_word(fname, fword)
